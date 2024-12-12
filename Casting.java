a6fb8c0222164b09f0c7f25798521f818f33fad2
Anton Cheban <antonio.cheban@gmail.com>
2024-12-12 17:46:00 +0100

    show this classes

--- Casting.java
+++ Casting.java
@@ -1,5 +1,9 @@
 package SelfStudy.TeamProject;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -7,13 +11,14 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Casting {
-    private Participant participant = new Participant();
     private String id;
     private String name;
     private String description;
     private String location;
     private List<Participant> participants = new ArrayList<>();
-    CastingMain castingMain = new CastingMain();
+    static final Logger LOGGER = LoggerFactory.getLogger(Casting.class);
+
+
     public Casting(String id, String name, String description, String location) {
         this.id = id;
         this.name = name;
@@ -25,19 +30,31 @@ public class Casting {
         if ((participant.getId() == null) || (participant.getId().isEmpty())) {
             System.out.println("You enter incorrect Id!");
 //            throw new IllegalArgumentException("You enter incorrect Id!");
+            LOGGER.warn("attempt to enter incorrect id");
         } else if ((participant.getName() == null) || (participant.getName().isEmpty())) {
             System.out.println("You enter incorrect Name!");
 //            throw new IllegalArgumentException("You enter incorrect Name!");
+            LOGGER.warn("attempt to enter incorrect Name");
         } else if ((participant.getStatus() == null) || (participant.getStatus().isEmpty())) {
             System.out.println("You enter incorrect Status!");
 //            throw new IllegalArgumentException("You enter incorrect Status!");
+            LOGGER.warn("attempt to enter incorrect Status");
         } else {
             participants.add(participant);
+            LOGGER.info("new participant was added");
         }
     }
 
-    public void updateStatus(String participantName, String newStatus) {
-
+    public void updateStatus(String newStatus, String id) {
+        Participant participant1 = null;
+        for (Participant participant: participants){
+            if (participant.getId().equals(id)) {
+                participant1 = participant;
+            }
+        }
+        participant1.setStatus(newStatus);
+        System.out.println("Status was changed");
+        LOGGER.info("status was changed");
     }
 
     public List<Participant> getParticipants() {
--- CastingMain.java
+++ CastingMain.java
@@ -6,14 +6,21 @@ import java.util.List;
 import java.util.Scanner;
 
 public class CastingMain {
-    private static Participant participant = new Participant();
+    static String id = "";
+    static String newStatus = "";
     public static void main(String[] args) {
 
         Casting casting = new Casting("a123", "Casting", "bla", "bla");
-        buildParticipant();
+        Participant participant = buildParticipant();
         casting.registerParticipant(participant);
+        participant = buildParticipant();
         casting.registerParticipant(participant);
         showParticipants(casting);
+
+        gettingNewStatus();
+        casting.updateStatus(newStatus,id);
+        showParticipants(casting);
+
     }
 
     private static void showParticipants(Casting casting) {
@@ -37,5 +44,13 @@ public class CastingMain {
         String status = scanner.nextLine();
         participant.setStatus(status);
         return participant;
+    }
+
+    private static void gettingNewStatus() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Participant Id: ");
+        id = scanner.nextLine();
+        System.out.println("Enter new status for this Participant: ");
+        newStatus = scanner.nextLine();
     }
 }
