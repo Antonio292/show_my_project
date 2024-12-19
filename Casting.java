ce7e840182ab901d751ebaff143dc81ba6e717bb
Anton Cheban <antonio.cheban@gmail.com>
2024-12-19 17:34:54 +0100

    # attempt to commit in new branch

--- Casting.java
+++ Casting.java
@@ -1,6 +1,8 @@
 package SelfStudy.TeamProject;
 
 
+import lombok.extern.flogger.Flogger;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,14 +11,15 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
-
+@Slf4j
 public class Casting {
+    private static final Logger log = LoggerFactory.getLogger(Casting.class);
     private String id;
     private String name;
     private String description;
     private String location;
     private List<Participant> participants = new ArrayList<>();
-    static final Logger LOGGER = LoggerFactory.getLogger(Casting.class);
+
 
 
     public Casting(String id, String name, String description, String location) {
@@ -30,18 +33,18 @@ public class Casting {
         if ((participant.getId() == null) || (participant.getId().isEmpty())) {
             System.out.println("You enter incorrect Id!");
 //            throw new IllegalArgumentException("You enter incorrect Id!");
-            LOGGER.warn("attempt to enter incorrect id");
+            log.warn("attempt to enter incorrect id");
         } else if ((participant.getName() == null) || (participant.getName().isEmpty())) {
             System.out.println("You enter incorrect Name!");
 //            throw new IllegalArgumentException("You enter incorrect Name!");
-            LOGGER.warn("attempt to enter incorrect Name");
+            log.warn("attempt to enter incorrect Name");
         } else if ((participant.getStatus() == null) || (participant.getStatus().isEmpty())) {
             System.out.println("You enter incorrect Status!");
 //            throw new IllegalArgumentException("You enter incorrect Status!");
-            LOGGER.warn("attempt to enter incorrect Status");
+            log.warn("attempt to enter incorrect Status");
         } else {
             participants.add(participant);
-            LOGGER.info("new participant was added");
+            log.info("new participant was added");
         }
     }
 
@@ -54,7 +57,7 @@ public class Casting {
         }
         participant1.setStatus(newStatus);
         System.out.println("Status was changed");
-        LOGGER.info("status was changed");
+        log.info("status was changed");
     }
 
     public List<Participant> getParticipants() {
